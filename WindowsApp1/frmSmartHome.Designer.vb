79ca0719ed21dd3546d8a07f72bcfc46bde5bdac
JulieRapson <rapson_julie@columbusstate.edu>
2023-04-09 21:40:03 -0400

    first draft, needs a lot more

---
+++ WindowsApp1/frmSmartHome.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSmartHome
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picSmartHome = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.picSmartHome, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picSmartHome
+        '
+        Me.picSmartHome.Image = Global.WindowsApp1.My.Resources.Resources.smarthome
+        Me.picSmartHome.Location = New System.Drawing.Point(1, 0)
+        Me.picSmartHome.Name = "picSmartHome"
+        Me.picSmartHome.Size = New System.Drawing.Size(349, 345)
+        Me.picSmartHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSmartHome.TabIndex = 0
+        Me.picSmartHome.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(416, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(312, 90)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Smart Home" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Electric Savings"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'frmSmartHome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Bisque
+        Me.ClientSize = New System.Drawing.Size(800, 601)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.picSmartHome)
+        Me.Name = "frmSmartHome"
+        Me.Text = "Smart Home Application"
+        CType(Me.picSmartHome, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picSmartHome As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
