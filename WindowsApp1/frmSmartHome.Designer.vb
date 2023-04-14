d54989f2b44572cd3e99eaff98375e4c11e6dc4d
JulieRapson <rapson_julie@columbusstate.edu>
2023-04-13 21:19:27 -0400

    Comments and code final alterations

--- WindowsApp1/frmSmartHome.Designer.vb
+++ WindowsApp1/frmSmartHome.Designer.vb
@@ -23,7 +23,12 @@ Partial Class frmSmartHome
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.picSmartHome = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.cboMonth = New System.Windows.Forms.ComboBox()
+        Me.lblSavingsMonth = New System.Windows.Forms.Label()
+        Me.btnDisplayStats = New System.Windows.Forms.Button()
+        Me.lblSavingsAvg = New System.Windows.Forms.Label()
+        Me.lblSavingsMax = New System.Windows.Forms.Label()
         CType(Me.picSmartHome, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -37,16 +42,71 @@ Partial Class frmSmartHome
         Me.picSmartHome.TabIndex = 0
         Me.picSmartHome.TabStop = False
         '
-        'Label1
+        'lblTitle
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Tahoma", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(416, 51)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(312, 90)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Smart Home" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Electric Savings"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(416, 51)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(312, 90)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Smart Home" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Electric Savings"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'cboMonth
+        '
+        Me.cboMonth.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboMonth.FormattingEnabled = True
+        Me.cboMonth.Location = New System.Drawing.Point(473, 185)
+        Me.cboMonth.Name = "cboMonth"
+        Me.cboMonth.Size = New System.Drawing.Size(207, 33)
+        Me.cboMonth.TabIndex = 2
+        '
+        'lblSavingsMonth
+        '
+        Me.lblSavingsMonth.AutoSize = True
+        Me.lblSavingsMonth.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSavingsMonth.Location = New System.Drawing.Point(213, 373)
+        Me.lblSavingsMonth.Name = "lblSavingsMonth"
+        Me.lblSavingsMonth.Size = New System.Drawing.Size(81, 25)
+        Me.lblSavingsMonth.TabIndex = 3
+        Me.lblSavingsMonth.Text = "Label2"
+        Me.lblSavingsMonth.Visible = False
+        '
+        'btnDisplayStats
+        '
+        Me.btnDisplayStats.BackColor = System.Drawing.Color.DodgerBlue
+        Me.btnDisplayStats.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplayStats.ForeColor = System.Drawing.Color.White
+        Me.btnDisplayStats.Location = New System.Drawing.Point(269, 416)
+        Me.btnDisplayStats.Name = "btnDisplayStats"
+        Me.btnDisplayStats.Size = New System.Drawing.Size(262, 43)
+        Me.btnDisplayStats.TabIndex = 4
+        Me.btnDisplayStats.Text = "Display Statistics"
+        Me.btnDisplayStats.UseVisualStyleBackColor = False
+        Me.btnDisplayStats.Visible = False
+        '
+        'lblSavingsAvg
+        '
+        Me.lblSavingsAvg.AutoSize = True
+        Me.lblSavingsAvg.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSavingsAvg.Location = New System.Drawing.Point(213, 482)
+        Me.lblSavingsAvg.Name = "lblSavingsAvg"
+        Me.lblSavingsAvg.Size = New System.Drawing.Size(81, 25)
+        Me.lblSavingsAvg.TabIndex = 5
+        Me.lblSavingsAvg.Text = "Label2"
+        Me.lblSavingsAvg.Visible = False
+        '
+        'lblSavingsMax
+        '
+        Me.lblSavingsMax.AutoSize = True
+        Me.lblSavingsMax.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSavingsMax.Location = New System.Drawing.Point(130, 532)
+        Me.lblSavingsMax.Name = "lblSavingsMax"
+        Me.lblSavingsMax.Size = New System.Drawing.Size(81, 25)
+        Me.lblSavingsMax.TabIndex = 6
+        Me.lblSavingsMax.Text = "Label3"
+        Me.lblSavingsMax.Visible = False
         '
         'frmSmartHome
         '
@@ -54,7 +114,12 @@ Partial Class frmSmartHome
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Bisque
         Me.ClientSize = New System.Drawing.Size(800, 601)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblSavingsMax)
+        Me.Controls.Add(Me.lblSavingsAvg)
+        Me.Controls.Add(Me.btnDisplayStats)
+        Me.Controls.Add(Me.lblSavingsMonth)
+        Me.Controls.Add(Me.cboMonth)
+        Me.Controls.Add(Me.lblTitle)
         Me.Controls.Add(Me.picSmartHome)
         Me.Name = "frmSmartHome"
         Me.Text = "Smart Home Application"
@@ -65,5 +130,10 @@ Partial Class frmSmartHome
     End Sub
 
     Friend WithEvents picSmartHome As PictureBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents cboMonth As ComboBox
+    Friend WithEvents lblSavingsMonth As Label
+    Friend WithEvents btnDisplayStats As Button
+    Friend WithEvents lblSavingsAvg As Label
+    Friend WithEvents lblSavingsMax As Label
 End Class
--- WindowsApp1/frmSmartHome.vb
+++ WindowsApp1/frmSmartHome.vb
@@ -1,3 +1,109 @@
-﻿Public Class frmSmartHome
+﻿'Author: Julie Rapson
+'Date: 4/13/2023
+'Program Name: Smart Home Electric Savings
+'Program Dscription: The program allows you to choose a month from the dropdown and it will tell you how much
+'savings were in that month selected.  If you click Display statistics button, it will give you the 
+'Average monthly savings as well as tell you the month with the MAX monthly savings. 
+
+Option Strict On
+Public Class frmSmartHome
+    'Declare private variables which includes the array
+    'The array is broken down by the size of it (0 starting and 12 months so 11)
+    Private _intSizeOfArray As Integer = 11
+    Private _strSavings(_intSizeOfArray) As String
+    Private _decBill(_intSizeOfArray) As Decimal
+    Private Sub frmSmartHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Declare variables needed to read file as well as variables for the savings amount
+        'and a conter to increment through the array.
+        Dim objReader As IO.StreamReader
+        Dim strSavingsAmount As String
+        Dim intCount As Integer = 0
+        Dim intFill As Integer
+        'Reset all labels to no text
+        lblSavingsMonth.Text = ""
+        lblSavingsMax.Text = ""
+        lblSavingsAvg.Text = ""
+
+        'If IO.File.Exists("C:\Users\myram\OneDrive\classes\GUID2023\Weeklyassignment8\text.txt") Then
+        'objReader = IO.File.OpenText("C:\Users\myram\OneDrive\classes\GUID2023\Weeklyassignment8\text.txt")
+
+        'I tried the Try/Catch but was not able to get it to work properly so I backed it out :( 
+
+        'I moved the text file into the debug folder so I could call it with no path.  Hoping this works outside of testing in my
+        'environment :) 
+        If IO.File.Exists("text.txt") Then
+            objReader = IO.File.OpenText("text.txt")
+            'read line by line
+            Do While objReader.Peek <> -1
+                _strSavings(intCount) = objReader.ReadLine()
+                strSavingsAmount = objReader.ReadLine()
+                _decBill(intCount) = Convert.ToDecimal(strSavingsAmount)
+                intCount += 1
+            Loop
+            objReader.Close()
+            'Fill combo box
+            For intFill = 0 To (_strSavings.Length - 1)
+                cboMonth.Items.Add(_strSavings(intFill))
+            Next
+        Else
+            MsgBox("The file is not available or the path is incorrect.", , "Error")
+            Close()
+        End If
+    End Sub
+
+    Private Sub cboMonth_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMonth.SelectedIndexChanged
+        ' When a selection is made on drop down, it will print out the savings amount for that selection.
+        'Also have to make the label visible and the button visible
+        Dim intSelectMonth As Integer
+        intSelectMonth = cboMonth.SelectedIndex
+        lblSavingsMonth.Visible = True
+        lblSavingsMonth.Text = "The electric savings for " & _strSavings(intSelectMonth) & " is $" & _decBill(intSelectMonth).ToString()
+        btnDisplayStats.Visible = True
+
+    End Sub
+
+    Private Sub btnDisplayStats_Click(sender As Object, e As EventArgs) Handles btnDisplayStats.Click
+        'Calling the subs to do the calculations and results display
+        CalcAvg()
+        CalcMax()
+    End Sub
+
+    Private Sub CalcAvg()
+        'This sub is to calculate the average month.
+        'Must iterate through the months to get the average
+        Dim decTotal As Decimal = 0D
+        Dim decAvg As Decimal = 0D
+        Dim intYr As Integer
+        Dim intNumYr As Integer
+
+        For Each intNumYr In _decBill
+            decTotal += _decBill(intYr)
+            intYr += 1
+        Next
+        decAvg = decTotal / Convert.ToDecimal(_decBill.Length())
+        'Make the lable visible here and set the text message for label
+        lblSavingsAvg.Visible = True
+        lblSavingsAvg.Text = "The average monthly savings: " & decAvg.ToString("C2")
+    End Sub
+    Private Sub CalcMax()
+        'This sub is to calculate the highest savings month.
+        '
+        Dim IntMonth As Integer
+        Dim IntMaxNum As Integer = 0
+        Dim IntSelection As Integer = 0
+        Dim strMaxMonth As String = ""
+
+        For Each IntMonth In _decBill
+            IntMaxNum = Math.Max(IntMaxNum, IntMonth)
+            If (IntMonth >= IntMaxNum) Then
+                strMaxMonth = _strSavings(IntSelection)
+            End If
+            IntSelection += 1
+        Next
+        'Make the lable visible here and set the text message for label
+        lblSavingsMax.Visible = True
+        lblSavingsMax.Text = strMaxMonth & " had the most significant monthly savings."
+    End Sub
+
 
 End Class
